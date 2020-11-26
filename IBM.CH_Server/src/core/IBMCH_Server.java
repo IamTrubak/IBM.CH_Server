895a93cb4b905d41c1bf64ce9bf63ab996dd2cff
Jakub Janšta <jansta.ja@gmail.com>
2020-11-26 20:13:31 +0100

    Update IBMCH_Server.java

--- IBM.CH_Server/src/core/IBMCH_Server.java
+++ IBM.CH_Server/src/core/IBMCH_Server.java
@@ -3,7 +3,7 @@ package core;
 public class IBMCH_Server {
 
 	public static void main(String[] args) {
-		System.out.println("Nothing to see here yet");
+		System.out.println("Nothing to see here yet!");
 
 	}
 
