5991677afb443bf3ede845fb40ff5296717d7f04
Jakub Janšta <jansta.ja@gmail.com>
2020-11-26 19:45:43 +0100

    Initial commit

---
+++ IBM.CH_Server/src/core/IBMCH_Server.java
@@ -0,0 +1,10 @@
+package core;
+
+public class IBMCH_Server {
+
+	public static void main(String[] args) {
+		System.out.println("Nothing to see here yet!");
+
+	}
+
+}
---
+++ IBM.CH_Server/src/networking/TCPServer.java
@@ -0,0 +1,15 @@
+package networking;
+
+public class TCPServer extends networking.Extasys.Network.TCP.Server.ExtasysTCPServer {
+
+	public TCPServer(String name, String description, int corePoolSize, int maximumPoolSize) {
+		super(name, description, corePoolSize, maximumPoolSize);
+		/*TODO
+		 * interface pro TCP server
+		 * override metody
+		 * správa pøipojení
+		 * */
+	}
+	
+	
+}
